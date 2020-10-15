19f7ad9e8c956eae1f9b52603851ce8775216bfa
publicqi <qisu@ucsb.edu>
2020-10-15 15:22:54 +0800

    Remove replicate assignments

--- pyscope/course.py
+++ pyscope/course.py
@@ -13,9 +13,11 @@ class GSCourse():
         self.assignments = []
 
     def load_assignments(self):
+        self.assignments = []
         assignment_resp = self.session.get(
             'https://www.gradescope.com/courses/' + self.cid)
-        parsed_assignment_resp = BeautifulSoup(assignment_resp.text, 'html.parser')
+        parsed_assignment_resp = BeautifulSoup(
+            assignment_resp.text, 'html.parser')
 
         data_table_tbody = parsed_assignment_resp.find('tbody')
         assignment_entries = list(data_table_tbody.findAll('tr'))
@@ -28,12 +30,16 @@ class GSCourse():
             else:
                 name = name_th.string
 
-            status = assignment.find('div', class_='submissionStatus--text').string
-            released_ts = int(parser.parse(assignment.findAll('td', class_='hidden-column')[0].string).timestamp())
-            due_ts = int(parser.parse(assignment.findAll('td', class_='hidden-column')[1].string).timestamp())
+            status = assignment.find(
+                'div', class_='submissionStatus--text').string
+            released_ts = int(parser.parse(assignment.findAll(
+                'td', class_='hidden-column')[0].string).timestamp())
+            due_ts = int(parser.parse(assignment.findAll(
+                'td', class_='hidden-column')[1].string).timestamp())
             current_ts = int(datetime.now().timestamp())
 
-            self.assignments.append({'name':name, 'status':status, 'released_ts':released_ts, 'due_ts':due_ts, 'current_ts':current_ts})
+            self.assignments.append(
+                {'name': name, 'status': status, 'released_ts': released_ts, 'due_ts': due_ts, 'current_ts': current_ts})
 
     def get_dues(self):
         self.load_assignments()
